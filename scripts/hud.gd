0663c35579dad8859a8261f506775e7d86490e5a
Michael Cantrell <macdude95@me.com>
2026-08-30 15:33:45 -0700

    Clarify wave counter label

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,7 +4,7 @@ func set_score(value: int) -> void:
 	$ScoreLabel.text = "SCORE  %04d" % value
 
 func set_wave(value: int) -> void:
-	$WaveLabel.text = "DEPTH  %02d" % value
+	$WaveLabel.text = "WAVE  %02d" % value
 
 func show_game_over(final_score: int) -> void:
 	$GameOverLabel.visible = true
